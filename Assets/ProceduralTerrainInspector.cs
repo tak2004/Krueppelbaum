3489d1c171345b2300b88af3ef3ccf2cac4a25d3
TAK2004 <thomas@ptscientists.com>
2017-08-04 16:32:14 +0200

    -initial commit

---
+++ Assets/ProceduralTerrainInspector.cs
@@ -0,0 +1,19 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(ProceduralBonzaiTerrain))]
+public class ProceduralTerrainInspector : Editor
+{
+     public override void OnInspectorGUI () {
+        DrawDefaultInspector();
+
+        ProceduralBonzaiTerrain myScript = (ProceduralBonzaiTerrain)target;
+        if (GUILayout.Button("Your ButtonText"))
+        {
+            myScript.GenerateHeights();
+        }
+    }
+ }
+#endif
